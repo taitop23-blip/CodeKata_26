29bb886f55d764f585f0f9baeefc8990d6e749bf
DESKTOP-F6FGIDC\TaitoP <taito.p23@gmail.com>
2026-04-09 09:13:43 +0900

    Add CodeKata Num26 file.

---
+++ CodeKata_26/CodeKata_26/main.cpp
@@ -0,0 +1,26 @@
+// main.cpp
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// absolutes_len은 배열 absolutes의 길이입니다.
+// signs_len은 배열 signs의 길이입니다.
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+
+    for (int i = 0; i < absolutes_len; ++i) {
+
+        if (signs[i] == true) {
+            answer += absolutes[i];
+        }
+
+        else {
+            answer -= absolutes[i];
+        }
+    }
+
+  
+        
+    return answer;
+}
